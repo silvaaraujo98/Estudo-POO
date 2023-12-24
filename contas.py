fb5740876066664bb8ea8100b41b19c3cc8901bd
unknown <jvsilvaaraujo98@gmail.com>
2023-12-24 12:52:42 -0300

    Escrevndo sobre Herança em POO

--- contas.py
+++ contas.py
@@ -26,3 +26,12 @@ class Conta:
         for o in self.operacoes:
             print(f"{o[0]:10s} {o[1]:10.2f}")
         print(f"\n Saldo: {self.saldo:10.2f}\n")
+class ContaEspecial(Conta):
+    def __init__(self, clientes, número, saldo=0,limite=0):
+        Conta.__init__(self,clientes,número,saldo)
+        self.limite = limite
+
+    def saque(self,valor):
+        if self.saldo + self.limite >= valor:
+            self.saldo-=valor
+            self.operacoes.append(["SAQUE",valor])
